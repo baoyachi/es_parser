62c01fa0f0cf6b1d53b4b8adcdae774169835d47
baoyachi <liaoymxsdl@gmail.com>
2021-11-17 13:24:31 +0800

    add key ext

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,27 @@
+use nom::error::ParseError;
+use nom::{AsChar, IResult, InputTakeAtPosition};
+
+pub fn key<T, E: ParseError<T>>(input: T) -> IResult<T, T, E>
+where
+    T: InputTakeAtPosition,
+    <T as InputTakeAtPosition>::Item: AsChar,
+{
+    input.split_at_position_complete(|item| {
+        let x = item.as_char();
+        !matches!(x, 'A'..='Z' | 'a'..='z' |'0'..='9'|'-'|'_')
+    })
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_key() {
+        fn key_demo(input: &str) -> IResult<&str, &str> {
+            let x = key(input)?;
+            Ok(x)
+        }
+        assert_eq!(Ok(("/tweet/1", "twitter")), key_demo("twitter/tweet/1"))
     }
 }
