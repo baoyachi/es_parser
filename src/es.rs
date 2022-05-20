10b621533aa2f4a6c54748448508c4410ceda9ac
step-baby <3528004749@qq.com>
2022-05-20 17:52:57 +0800

    fix: Determine the ES request type

--- src/es.rs
+++ src/es.rs
@@ -1,22 +1,90 @@
-use crate::{key, slash};
+use crate::{decomposition, key, slash};
 use nom::sequence::tuple;
 use nom::IResult;
 
 #[derive(Debug, PartialEq)]
-enum ReqType {
-    IndexAPI,
+pub enum ReqType {
     Search,
+    SearchTemplate,
+    MultiSearch,
+    MultiSearchTemplate,
+    MGet,
+    IndexApi,
+    IndexBlockApi,
+    IndexTemplate,
+    DeleteByQuery,
+    UpdateByQuery,
+    Tasks,
+    Bulk,
+    Ingest,
+    Refresh,
+    Reindex,
+    Termvectors,
+    MTermvectors,
+    SearchShards,
+    ScriptsTemplate,
+    Validate,
+    Explain,
+    FieldStats,
+    FieldCaps,
+    Cat,
+    Cluster,
+    Nodes,
+    Remote,
+    Analyze,
+    Xpark,
+    Security,
+    Watcher,
+    Migration,
+    All,
+    AllSettings,
+    Template,
+    Other,
+    None
 }
 
-fn req_type(input: &str) -> IResult<&str, ReqType> {
-    let (input, (_, out)) = tuple((slash, key))(input)?;
-    let req_type = match out {
-        //TODO replace with from_str
-        "_msearch" => ReqType::Search,
-        _ => ReqType::IndexAPI,
+fn req_type(input: &str) -> IResult<Vec<String>, ReqType> {
+    let (out, func) = decomposition(input);
+
+    let req_type = if let Some(value) = func {
+        match value.as_ref() {
+            "_search" => ReqType::Search,
+            "_msearch" => ReqType::MultiSearch,
+            "_template" => ReqType::Template,
+            "_tasks" => ReqType::Tasks,
+            "_ingest" => ReqType::Ingest,
+            "_refresh" => ReqType::Refresh,
+            "_reindex" => ReqType::Reindex,
+            "_scripts" => ReqType::ScriptsTemplate,
+            "_field_stats" => ReqType::FieldStats,
+            "_field_caps" => ReqType::FieldCaps,
+            "_all" => ReqType::All,
+            "_block" => ReqType::IndexBlockApi,
+            "_bulk" => ReqType::Bulk,
+            "_mget" => ReqType::MGet,
+            "_delete_by_query" => ReqType::DeleteByQuery,
+            "_update_by_query" => ReqType::UpdateByQuery,
+            "_termvectors" => ReqType::Termvectors,
+            "_mtermvectors" => ReqType::MTermvectors,
+            "_search_shards" => ReqType::SearchShards,
+            "_explain" => ReqType::Explain,
+            "_cat" => ReqType::Cat,
+            "_cluster" => ReqType::Cluster,
+            "_nodes" => ReqType::Nodes,
+            "_remote" => ReqType::Remote,
+            "_analyze" => ReqType::Analyze,
+            "_xpack" => ReqType::Xpark,
+            _ => ReqType::IndexApi,
+        }
+    }else {
+        ReqType::None
     };
-    Ok((input, req_type))
+
+    Ok((out, req_type))
 }
+
+
+// search_url = {("/_search"|(virgule* ~ index* ~ "/_search"))}
 
 #[cfg(test)]
 mod tests {
@@ -24,7 +92,7 @@ mod tests {
 
     #[test]
     fn test_req_type() {
-        let x = req_type("_msearch").unwrap();
-        assert_eq!(("", ReqType::Search), x)
+        let x = req_type("/_search").unwrap();
+        assert_eq!((vec!["/_search".to_string()], ReqType::Search), x)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
 mod es;
 
 use nom::error::ParseError;
-use nom::{AsChar, IResult, InputTakeAtPosition};
+use nom::{AsChar, IResult, InputTakeAtPosition, InputTake, FindSubstring};
+use nom::bytes::complete::{take_till, take_until};
 
 pub fn key<T, E: ParseError<T>>(input: T) -> IResult<T, T, E>
 where
@@ -19,6 +20,28 @@ where
     <T as InputTakeAtPosition>::Item: AsChar,
 {
     input.split_at_position_complete(|item| !matches!(item.as_char(), '/'))
+}
+
+pub fn decomposition(input: &str) -> (Vec<String>, Option<String>){
+    if input.eq("/") {
+        return (vec!["/".to_string()], None)
+    }
+
+    let mut func = None;
+    let result: Vec<&str> = input.split('/').collect();
+    let mut values = Vec::new();
+    for value in result {
+        if value.is_empty() {
+            continue;
+        }
+
+        if func.is_none() && value.starts_with("_"){
+            func = Some(value.to_string());
+        }
+
+        values.push(format!("/{}", value));
+    }
+    (values, func)
 }
 
 #[cfg(test)]
@@ -40,6 +63,15 @@ mod tests {
             let x = slash(input)?;
             Ok(x)
         }
-        assert_eq!(Ok(("", "/")), slash_fn("/"))
+        assert_eq!(Ok(("", "/")), slash_fn("/"));
+    }
+
+    #[test]
+    fn test_decomposition() {
+        assert_eq!(vec!["/"], decomposition("/").0);
+
+        assert_eq!(vec!["/_search"], decomposition("/_search").0);
+
+        assert_eq!(vec!["/incident", "/_search"], decomposition("/incident/_search").0);
     }
 }
